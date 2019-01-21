6b6960b02d9b9abec38360e60f7d17bf95c49bf7
jonchang03 <jonchang03@gmail.com>
2019-01-21 11:30:10 -0500

    passed example for MP0

--- MP0/MP0.java
+++ MP0/MP0.java
@@ -4,6 +4,9 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
 public class MP0 {
     Random generator;
     String userName;
@@ -38,11 +41,92 @@ public class MP0 {
 
     public String[] process() throws Exception{
     	String[] topItems = new String[20];
-        Integer[] indexes = getIndexes();
+        Integer[] indices = getIndexes(); // rename from indexes
 
-    	//TO DO
+        //TO DO
+        // Read input from stdin into list of sentences (ArrayList)
+        // https://docs.oracle.com/javase/8/docs/api/java/util/Scanner.html
+        List<ArrayList<String>> inputSentences = new ArrayList<ArrayList<String>>();
+        Scanner sc = new Scanner(System.in);
+        String sentence = "";
+        while (sc.hasNextLine()) {
+            sentence = sc.nextLine();
+            /**
+             * 1. Divide each sentence into a list of words using delimiters provided 
+             * in the “delimiters” variable.
+             */
+            ArrayList<String> words = new ArrayList<String>();
+            StringTokenizer st = new StringTokenizer(sentence, delimiters);
+            String token = "";
+            while (st.hasMoreTokens()) {
+                // System.out.println(st.nextToken());
+                /**
+                 * 2. Make all the tokens lowercase and remove any tailing and leading spaces.
+                 */
+                token = st.nextToken().toLowerCase().trim();
 
+                /**
+                 * 3. Ignore all common words provided in the “stopWordsArray” variable.
+                 */
+                if(!Arrays.stream(stopWordsArray).anyMatch(token::equals)) {
+                    words.add(token);
+                }
+            }
 
+            inputSentences.add(words);
+        }
+        sc.close();
+        // System.out.println(inputSentences.size());
+        // for (ArrayList<String> sentences : inputSentences) {
+        //     for (String word : sentences) {
+        //         System.out.println(word);
+        //     }
+        // }
+        // int max = Collections.max(Arrays.asList(indices)); 
+        // System.out.println(max);
+
+        /**
+         * 4. Keep track of word frequencies. To make the application more interesting, 
+         * you have to process only the titles with certain indexes. These indexes are 
+         * accessible using the “getIndexes” method, which returns an Integer Array with
+         * 0-based indexes to the input file. It is possible to have an index appear
+         * several times. In this case, just process the index multiple times.
+         */
+
+        // use map with word as key and count as value
+        Map<String, Integer> frequencyCounter = new HashMap<>();
+        // loop through the indices
+        for (int i : indices) {
+            ArrayList<String> words = inputSentences.get(i);
+            for (String w : words) {
+                Integer n = frequencyCounter.get(w);
+                n = (n == null) ? 1 : ++n;
+                frequencyCounter.put(w, n);
+            }
+        }
+
+        /** 
+         * 5. Sort the words by frequency in a descending order. If two words have the 
+         * same number count, use the lexigraphy.
+         */
+        // https://www.javacodegeeks.com/2017/09/java-8-sorting-hashmap-values-ascending-descending-order.html
+        Map<String, Integer> sortedMap = frequencyCounter
+            .entrySet()
+            .stream()
+            .sorted(Collections.reverseOrder(Entry.comparingByValue()))
+            .collect(Collectors.toMap(Entry::getKey, Entry::getValue,
+                           (o1, o2) -> o1, LinkedHashMap::new));
+        
+        /**
+         * 6. Print out the top 20 items from the sorted list as a String Array.
+         */
+        Set<String> keys = sortedMap.keySet();
+        String[] keysArray = keys.toArray(new String[keys.size()]);
+        for(int i=0; i<keysArray.length && i<20; i++) {
+            // System.out.println(keysArray[i]);
+            topItems[i] = keysArray[i];
+        }
+                                    
 		return topItems;
     }
 
@@ -51,7 +135,9 @@ public class MP0 {
     		System.out.println("missing the argument");
     	}
     	else{
-    		String userName = args[0];
+            // used as a seed for random number generator to create indices
+            String userName = args[0]; 
+            
 	    	MP0 mp = new MP0(userName);
 	    	String[] topItems = mp.process();
 
