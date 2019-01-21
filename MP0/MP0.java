a84066614eb9477a5792bbc5a3564161975fe31d
jonchang03 <jonchang03@gmail.com>
2019-01-21 11:27:31 -0500

    added boilerplate code for MP0

---
+++ MP0/MP0.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+
+public class MP0 {
+    Random generator;
+    String userName;
+    String delimiters = " \t,;.?!-:@[](){}_*/";
+    String[] stopWordsArray = {"i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours",
+            "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its",
+            "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that",
+            "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having",
+            "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+            "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+            "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+            "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+            "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+            "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"};
+
+    public MP0(String userName) {
+        this.userName = userName;
+    }
+
+
+    public Integer[] getIndexes() throws NoSuchAlgorithmException {
+        Integer n = 10000;
+        Integer number_of_lines = 50000;
+        Integer[] ret = new Integer[n];
+        long longSeed = Long.parseLong(this.userName);
+        this.generator = new Random(longSeed);
+        for (int i = 0; i < n; i++) {
+            ret[i] = generator.nextInt(number_of_lines);
+        }
+        return ret;
+    }
+
+    public String[] process() throws Exception{
+    	String[] topItems = new String[20];
+        Integer[] indexes = getIndexes();
+
+    	//TO DO
+
+
+		return topItems;
+    }
+
+    public static void main(String args[]) throws Exception {
+    	if (args.length < 1){
+    		System.out.println("missing the argument");
+    	}
+    	else{
+    		String userName = args[0];
+	    	MP0 mp = new MP0(userName);
+	    	String[] topItems = mp.process();
+
+	        for (String item: topItems){
+	            System.out.println(item);
+	        }
+	    }
+	}
+
+}
---
+++ MP0/README.md
@@ -0,0 +1,4 @@
+# MP0
+A Machine Programming Assignment for CS498: Cloud Computing Applications UIUC
+Spring 2018
+MP0 
