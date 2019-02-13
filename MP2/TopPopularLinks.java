1624ff64a31f363214cc180d005ca637c0112bb4
Jonathan Chang <jchang16@LAMU02XG4EHJG5J.uhc.com>
2019-02-13 10:25:02 -0500

    finished MP2 Exercise D

--- MP2/TopPopularLinks.java
+++ MP2/TopPopularLinks.java
@@ -24,6 +24,11 @@ import java.util.StringTokenizer;
 import java.util.TreeSet;
 
 public class TopPopularLinks extends Configured implements Tool {
+
+    // ADD
+    public static final Log LOG = LogFactory.getLog(TopPopularLinks.class);
+    public static final int N = 10;
+    // END ADD
     public static void main(String[] args) throws Exception {
         int res = ToolRunner.run(new Configuration(), new TopPopularLinks(), args);
         System.exit(res);
@@ -32,6 +37,43 @@ public class TopPopularLinks extends Configured implements Tool {
     @Override
     public int run(String[] args) throws Exception {
         //TODO
+        Configuration conf = this.getConf();
+        FileSystem fs = FileSystem.get(conf);
+        Path tmpPath = new Path("./tmp");
+        fs.delete(tmpPath, true);
+
+        Job jobA = Job.getInstance(conf, "Popular Links Count");
+        jobA.setOutputKeyClass(IntWritable.class);
+        jobA.setOutputValueClass(IntWritable.class);
+
+        jobA.setMapperClass(LinkCountMap.class);
+        jobA.setReducerClass(LinkCountReduce.class);
+
+        FileInputFormat.setInputPaths(jobA, new Path(args[0]));
+        FileOutputFormat.setOutputPath(jobA, tmpPath);
+
+        jobA.setJarByClass(TopPopularLinks.class);
+        jobA.waitForCompletion(true);
+
+        Job jobB = Job.getInstance(conf, "Top Popular Links");
+        jobB.setOutputKeyClass(IntWritable.class);
+        jobB.setOutputValueClass(IntWritable.class);
+
+        jobB.setMapOutputKeyClass(NullWritable.class);
+        jobB.setMapOutputValueClass(IntArrayWritable.class);
+
+        jobB.setMapperClass(TopLinksMap.class);
+        jobB.setReducerClass(TopLinksReduce.class);
+        jobB.setNumReduceTasks(1);
+
+        FileInputFormat.setInputPaths(jobB, tmpPath);
+        FileOutputFormat.setOutputPath(jobB, new Path(args[1]));
+
+        jobB.setInputFormatClass(KeyValueTextInputFormat.class);
+        jobB.setOutputFormatClass(TextOutputFormat.class);
+
+        jobB.setJarByClass(TopPopularLinks.class);
+        return jobB.waitForCompletion(true) ? 0 : 1;
     }
 
     public static class IntArrayWritable extends ArrayWritable {
@@ -51,10 +93,32 @@ public class TopPopularLinks extends Configured implements Tool {
 
     public static class LinkCountMap extends Mapper<Object, Text, IntWritable, IntWritable> {
         //TODO
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            //TODO
+            String line = value.toString();
+            StringTokenizer tokenizer = new StringTokenizer(line, " :");
+            IntWritable page = new IntWritable(Integer.parseInt(tokenizer.nextToken().trim()));
+            context.write(page, new IntWritable(0));
+
+			while (tokenizer.hasMoreTokens()) {
+                String nextToken = tokenizer.nextToken().trim();
+                IntWritable linkedPage = new IntWritable(Integer.parseInt(nextToken));
+                context.write(linkedPage, new IntWritable(1));
+			}
+        }
     }
 
     public static class LinkCountReduce extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
         //TODO
+        @Override
+        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			context.write(key, new IntWritable(sum));
+        }
     }
 
     public static class TopLinksMap extends Mapper<Text, Text, NullWritable, IntArrayWritable> {
@@ -65,6 +129,31 @@ public class TopPopularLinks extends Configured implements Tool {
         }
 
        //TODO
+       private TreeSet<Pair<Integer, Integer>> countToLinkMap = new TreeSet<Pair<Integer, Integer>>();
+       
+       @Override
+       public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
+           //TODO
+           Integer count = Integer.parseInt(value.toString());
+           Integer link = Integer.parseInt(value.toString());
+
+           countToLinkMap.add(new Pair<Integer, Integer>(count, link));
+           if (countToLinkMap.size() > N) {
+               countToLinkMap.remove(countToLinkMap.first());
+           }
+       }
+
+       @Override
+       // aggregate all map output into one reduce key
+       protected void cleanup(Context context) throws IOException, InterruptedException {
+           //TODO
+           for (Pair<Integer, Integer> item : countToLinkMap) {
+               Integer[] link = {item.second, item.first};
+               IntArrayWritable val = new IntArrayWritable(link);
+               context.write(NullWritable.get(), val);
+           }
+       }
+
     }
 
     public static class TopLinksReduce extends Reducer<NullWritable, IntArrayWritable, IntWritable, IntWritable> {
@@ -74,6 +163,28 @@ public class TopPopularLinks extends Configured implements Tool {
             Configuration conf = context.getConfiguration();
         }
         //TODO
+        private TreeSet<Pair<Integer, Integer>> countToLinkMap = new TreeSet<Pair<Integer, Integer>>();
+
+        @Override
+        public void reduce(NullWritable key, Iterable<IntArrayWritable> values, Context context) throws IOException, InterruptedException {
+            //TODO
+            for (IntArrayWritable val : values) {
+				IntWritable[] pair = (IntWritable[]) val.toArray();
+				Integer link = pair[0].get();
+				Integer count = pair[1].get();
+
+				countToLinkMap.add(new Pair<Integer, Integer>(count, link));
+				if (countToLinkMap.size() > N) {
+					countToLinkMap.remove(countToLinkMap.first());
+				}
+			}
+
+			for (Pair<Integer, Integer> item : countToLinkMap) {
+				IntWritable link = new IntWritable(item.second);
+				IntWritable value = new IntWritable(item.first);
+				context.write(link, value);
+			}
+        }
 
     }
 }
